b02d6496bed11ece41a49d2673b31257471207ce
DanielOmotesho <danielomotesho11@gmail.com>
2026-03-31 18:59:29 +0100

    Update jumblewords.py

--- jumblewords.py
+++ jumblewords.py
@@ -5,13 +5,17 @@ import random
 main = Tk()
 main.geometry("400x400")
 main.title("Jumble words game")
-main.config(bg = "black")
+main.config(bg = "white")
 
+scor_num = 0
 
 words = {
     "apple":"papel",
     "basket":"skabte",
-    "flashlight":"ghlaftlihs"}
+    "flashlight":"ghlaftlihs",
+    "bottle":"ttelob",
+    "olive":"eloiv",
+    "computer":"rmocptue"}
 
 correct_wrd = ""
 jumbled_wrd = ""
@@ -22,11 +26,22 @@ def random_wrd():
     jumble_wrd_l.config (text =jumbled_wrd )
 
 def check_answer():
-    global correct_wrd
+    global correct_wrd, scor_num
     guess = eb.get()
     if guess == correct_wrd:
         messagebox.showinfo("correct","you are correct")
+        scor_num += 1
+        scor.config(text= "SCORE:"+ str(scor_num))
+    
+        random_wrd()
+    else:
+        scor_num -= 1
+        scor.config(text = "SCORE:" + str(scor_num))
 
+
+
+scor = Label(main, text = "SCORE:" )
+scor.place(x=250, y=250)
 
 t = Label(main , text = "Jumble words game", font = 20)
 t.pack()
@@ -37,10 +52,10 @@ jumble_wrd_l.pack(pady= 30)
 eb = Entry(main)
 eb.pack(pady = 40)
 
-check_btn = Button(main, text = "CHECK",fg = "green")
+check_btn = Button(main, text = "CHECK",fg = "green", command = check_answer)
 check_btn.pack(pady = 30)
 
-reset_btn = Button(main, text = "RESET")
+reset_btn = Button(main, text = "RESET", command = random_wrd)
 reset_btn.pack (pady = 10)
 
 random_wrd()
